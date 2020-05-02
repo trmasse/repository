56f8ae26895a4c1dd0c7eedc0e1ad3deb8631e4e
Trevor <5540365+trmasse@users.noreply.github.com>
2020-05-01 20:58:58 -0400

    third commit; added MinHeap

--- repository.py
+++ repository.py
@@ -42,6 +42,7 @@ class LinkedList:
             current_node = current_node.get_link_node()
             
 #Hash Map with Separate Chaining (4/30/2020)
+#Python has built-in HashMaps called Dictionaries
 class HashMap:
   def __init__(self, size):
     self.array_size = size
@@ -89,5 +90,74 @@ class TreeNode:
             print(current_node.value)
             nodes_to_visit += current_node.children
             
+#Min Heap (5/1/2020)
+class MinHeap:
+    def __init__(self):
+        self.heap_list = [None]
+        self.count = 0
+        
+    def parent_idx(self, idx):
+        return idx // 2
+        
+    def left_child_idx(self, idx):
+        return idx * 2
+        
+    def right_child_idx(self, idx):
+        return idx * 2 + 1
+        
+    def child_present(self, idx):
+        return self.left_child_idx(idx) <= self.count
+    
+    def retrieve_min(self):
+        if self.count == 0:
+            print("No items in heap")
+            return None
+        min = self.heap_list[1]
+        self.heap_list[1] = self.heap_list[self.count]
+        self.count -= 1
+        self.heap_list.pop()
+        self.heapify_down()
+        return min
+        
+    def add(self, element):
+        self.count += 1
+        self.heap_list.append(element)
+        self.heapify_up()
+        
+    def get_smaller_child_idx(self, idx):
+        if self.right_child_idx(idx) > self.count:
+            return self.left_child_idx(idx)
+        else:
+            left_child = self.heap_list[self.left_child_idx(idx)]
+            right_child = self.heap_list[self.right_child_idx(idx)]
+            if left_child < right_child:
+                return self.left_child_idx(idx)
+            else:
+                return self.right_child_idx(idx)
+                
+    def heapify_up(self):
+        idx = self.count
+        while self.parent_idx(idx) > 0:
+            child = self.heap_list[idx]
+            parent = self.heap_list[self.parent_idx(idx)]
+            if parent > child:
+                self.heap_list[idx] = parent
+                self.heap_list[self.parent_idx(idx)] = child
+            idx = self.parent_idx(idx)
+            
+    def heapify_down(self):
+        idx = 1
+        while self.child_present(idx):
+            smaller_child_idx = self.get_smaller_child_idx(idx)
+            child = self.heap_list[smaller_child_idx]
+            parent = self.heap_list[idx]
+            if parent > child:
+                self.heap_list[idx] = child
+                self.heap_list[smaller_child_idx] = parent
+            idx = smaller_child_idx
+            
+        
+        
+    
             
     
