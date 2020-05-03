5a64b2804efe16c051ce51f1741866c72beb85f2
Trevor <5540365+trmasse@users.noreply.github.com>
2020-05-03 10:52:31 -0400

    fourth commit; added Graph (directed, weighted)

--- repository.py
+++ repository.py
@@ -156,6 +156,46 @@ class MinHeap:
                 self.heap_list[smaller_child_idx] = parent
             idx = smaller_child_idx
             
+#Directed Graph with Weights (5/3/2020)
+class Vertex:
+    def __init__(self, value):
+        self.value = value
+        self.edges = {}
+        
+    def add_edge(self, vertex, weight = 0):
+        self.edges[vertex] = weight
+        
+    def get_edges(self):
+        return list(self.edges.keys())
+        
+class Graph:
+    def __init__(self, directed = False):
+        self.graph_dict = {}
+        self.directed = directed
+    
+    def add_vertex(self, vertex):
+        self.graph_dict[vertex.value] = vertex
+        
+    def add_edge(self, from_vertex, to_vertex, weight = 0):
+        self.graph_dict[from_vertex.value].add_edge(to_vertex.value, weight)
+        if not self.directed:
+            self.graph_dict[to_vertex.value].add_edge(from_vertex.value, weight)
+            
+    def find_path(self, start_vertex, end_vertex):
+        start = [start_vertex]
+        seen = {}
+        while len(start) > 0:
+            current_vertex = start.pop(0)
+            seen[current_vertex] = True
+            if current_vertex == end_vertex:
+                return True
+            else:
+                vertex = self.graph_dict[current_vertex]
+                next_vertices = vertex.get_edges()
+                next_vertices = [vertex for vertex in next_vertices if vertex not in seen]
+                start.extend(next_vertices)
+        return False
+            
         
         
     
