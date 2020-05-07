b4f0041500b035075effb723abd212ad0add71e6
Trevor <5540365+trmasse@users.noreply.github.com>
2020-05-07 07:51:14 -0400

    7th commit; added QuickSort

--- repository.py
+++ repository.py
@@ -241,6 +241,28 @@ def merge(left, right):
         
     return result
         
+#Quicksort (alters original list) (5/7/2020)
+from random import randrange
+
+def quicksort(list, start, end):
+    if start >= end:
+        return
+    
+    pivot_idx = randrange(start, end + 1)
+    pivot_element = list[pivot_idx]
+    
+    list[end], list[pivot_idx] = list[pivot_idx], list[end]
+    
+    less_than_pointer = start
+    
+    for i in range(start, end):
+        if list[i] < pivot_element:
+            list[i], list[less_than_pointer] = list[less_than_pointer], list[i]
+            less_than_pointer += 1
+    list[end], list[less_than_pointer] = list[less_than_pointer], list[end]
+    
+    quicksort(list, start, less_than_pointer - 1)
+    quicksort(list, less_than_pointer + 1, end)
     
             
     
