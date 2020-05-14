85339beef76035b6562f62a0f5ffb2c2b4fb9616
Trevor <5540365+trmasse@users.noreply.github.com>
2020-05-14 08:12:08 -0400

    11th commit; added DFS & BFS graph search algorithms

--- repository.py
+++ repository.py
@@ -1,5 +1,5 @@
 #Data Structure Repository
-#Contents: Node, LinkedList, HashMap, Tree, MinHeap, Graph (Directed, Weighted), BubbleSort
+#Contents: Node, LinkedList, HashMap, Tree, MinHeap, Graph (Directed, Weighted), BubbleSort, MergeSort, Quicksort, RadixSort, LinearSearch, BinarySearch, DepthFirstSearch, 
 
 #Node (4/30/2020)
 class Node:
@@ -379,8 +379,42 @@ def binary_search3(data, search_val):
                 first = mid + 1
     print("{0} is not in the dataset".format(search_val))
     
-
-
+#DepthFirstSearch Graph Search Algorithm
+#Uses a python dictionary for the graph, values as set() containing the edges
+def dfs(graph, current_vertex, target_value, visited = None):
+    if not visited:
+        visited = []
+        
+    visited.append(current_vertex)
+    
+    if current_vertex == target_value:
+        return visited
+        
+    for neighbor in graph[current_vertex]:
+        if neighbor not in visited:
+            path = dfs(graph, neighbor, target_value, visited)
+            if path:
+                return path
+                
+#BreadthFirstSearch Graph Search Algorighm
+#Uses a python dictionary for the graph, values as set() containing the edges
+def bfs(graph, start_vertex, target_value):
+    path = [start_vertex]
+    vertex_and_path = [start_vertex, path]
+    bfs_queue = [vertex_and_path]
+    visited = set()
+    
+    while bfs_queue:
+        current_vertex, path = bfs_queue.pop(0)
+        visited.add(current_vertex)
+        
+        for neighbor in graph[current_vertex]:
+            if neighbor not in visited:
+                if neighbor == target_value:
+                    return path + [neighbor]
+                else:
+                    bfs_queue.append([neighbor, path + [neighbor]])
+                
 
 
 
